a4ccba9383e7815b78f1d54f449f356cbf2b45d2
asaad <asaad@trioplus-eg.com>
2016-03-19 14:00:04 +0200

    Edit Category

--- sm-shop/src/main/java/com/salesmanager/web/admin/controller/categories/CategoryController.java
+++ sm-shop/src/main/java/com/salesmanager/web/admin/controller/categories/CategoryController.java
@@ -287,7 +287,7 @@ public class CategoryController {
 				entry.put("code", category.getCode());
 				entry.put("visible", category.isVisible());
 				entry.put("sortOrder", category.getSortOrder());
-				entry.put("parentCategory", category.getParent().getDescription().getName());
+				entry.put("parentCategory",category.getParent()!=null? category.getParent().getDescription().getName():"");
 				resp.addDataEntry(entry);
 				
 				
