d69d5a4fe022c57c06390b8f6afa8ca5e5e433af
ayman mostafa <ayman.eltayary@gmail.com>
2016-05-29 00:17:19 +0200

    commit

--- sm-process/src/main/java/com/trioplus/sm/process/dao/MainDAO.java
+++ sm-process/src/main/java/com/trioplus/sm/process/dao/MainDAO.java
@@ -1,13 +1,14 @@
 package com.trioplus.sm.process.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.trioplus.sm.plus.process.exception.ProcessException;
 import org.springframework.transaction.annotation.Transactional;
 import com.trioplus.sm.process.model.CustomerShareCount;
-
+import com.trioplus.sm.process.model.CustomerShareLog;
 import com.trioplus.sm.process.model.CustomerSocialShareDiscount;
 import com.trioplus.sm.process.model.MerchantConfiguration;
 import com.trioplus.sm.process.model.SocialMediaConfig;
@@ -16,11 +17,14 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
+import javax.sql.DataSource;
+
 import org.hibernate.Criteria;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Restrictions;
+
 
 @Repository("mainDao")
 @Transactional
@@ -28,13 +32,21 @@ public class MainDAO {
 
 	@Autowired
 	private SessionFactory sessionFactory;
+	
+	private JdbcTemplate jdbcTemplate;
 
+
+	@Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+	
 	protected Session getSession() {
 		return sessionFactory.getCurrentSession();
 	}
 
 	/**
-	 * Get Social share config
+	 * Get Social share configuration
 	 */
 
 	public SocialMediaConfig getConfig(Long merchantId, String key) throws ProcessException {
@@ -55,7 +67,7 @@ public class MainDAO {
 	}
 
 	/**
-	 * Get customers list eligible to have social sharing discount
+	 * Get customers list eligible to  social sharing discount
 	 * 
 	 * @param config
 	 * @return
@@ -76,6 +88,15 @@ public class MainDAO {
 
 	}
 
+	/**
+	 * Insert record in CUSTOMER_SOCIAL_DISCOUNT_HISTORY
+	 * @param customer
+	 * @param config
+	 */
+	
+	
+	
+	
 	public void updateCustomer(CustomerShareCount customer, SocialMediaConfig config) {
 
 		CustomerSocialShareDiscount entity = new CustomerSocialShareDiscount();
@@ -103,6 +124,23 @@ public class MainDAO {
 		getSession().persist(entity);
 	
 	
+		
+		Criteria criteria = getSession().createCriteria(CustomerShareLog.class);
+		criteria.add(Restrictions.eq("customerId", entity.getCustomerId()));
+		criteria.add(Restrictions.eq("discountSent",false));
+		
+		List<CustomerShareLog> CustomerSahreList = criteria.list();
+		
+		for(CustomerShareLog shareLog :CustomerSahreList){
+			
+			shareLog.setDiscountSent(true);
+			getSession().saveOrUpdate(shareLog);
+			
+			
+		}
+		
+		
+		
 	}
 
 }
