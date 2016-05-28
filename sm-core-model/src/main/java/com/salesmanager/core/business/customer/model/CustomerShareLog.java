8b401c07972c2b37319367664613fe8634e0fec4
ayman mostafa <ayman.eltayary@gmail.com>
2016-05-28 02:14:59 +0200

    commit

---
+++ sm-core-model/src/main/java/com/salesmanager/core/business/customer/model/CustomerShareLog.java
@@ -0,0 +1,77 @@
+/**
+ * 
+ */
+package com.salesmanager.core.business.customer.model;
+
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.salesmanager.core.business.generic.model.SalesManagerEntity;
+
+/**
+ * @author Administrator
+ *
+ */
+
+@Entity
+@Table(name="CUSTOMER_SHARE_LOG",schema="lavetrina")
+public class CustomerShareLog extends SalesManagerEntity<Long, CustomerShareLog> {
+
+@Id
+@Column(name = "TRANSACTION_ID", unique=true, nullable=false)
+@GeneratedValue(strategy=GenerationType.IDENTITY)
+private Long id;
+public Long getId() {
+		return id;
+	}
+public void setId(Long id) {
+		this.id = id;
+	}
+@Column(name="CUSTOMER_ID")
+private long customerId;
+public long getCustomerId() {
+	return customerId;
+}
+public void setCustomerId(long customerId) {
+	this.customerId = customerId;
+}
+public Date getTransactionDate() {
+	return transactionDate;
+}
+public void setTransactionDate(Date transactionDate) {
+	this.transactionDate = transactionDate;
+}
+public int getChannel() {
+	return channel;
+}
+public boolean isDiscountSent() {
+	return discountSent;
+}
+public void setDiscountSent(boolean discountSent) {
+	this.discountSent = discountSent;
+}
+public void setChannel(int channel) {
+	this.channel = channel;
+}
+@Column(name="TRANSACTION_DATE")
+@DateTimeFormat(pattern = "dd/MM/yyyy")
+private Date transactionDate;
+@Column(name="CHANNEL")
+private int channel;
+
+
+@Column(name="DISCOUNT_SENT")
+private boolean discountSent;
+
+
+
+}
